7353d3d9c8dc8ce2fd5fd7ff6a660d5fd5f84d72
Ricardo <rockobaschirotto@gmail.com>
2023-11-20 14:34:53 -0300

    OrderStatus Enum

--- src/main/java/com/rocko/crudSpring/entities/Order.java
+++ src/main/java/com/rocko/crudSpring/entities/Order.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+import com.rocko.crudSpring.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -16,8 +16,8 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_order")
-public class Order implements Serializable{
-    
+public class Order implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -29,12 +29,15 @@ public class Order implements Serializable{
     @JoinColumn(name = "client_id")
     private User client;
 
+    private Integer orderStatus;
+
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -62,6 +65,16 @@ public class Order implements Serializable{
         this.client = client;
     }
 
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -86,9 +99,5 @@ public class Order implements Serializable{
             return false;
         return true;
     }
-
-    
-
-    
 
 }
